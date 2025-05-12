5b7047d8baad38ae90b3b7f53ec58d1052fe7512
cookiebanner2025 <analyticsmaster2@gmail.com>
2025-05-12 23:58:36 +0600

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,32 +1,57 @@
 const EU_COUNTRIES = [
-    'AT', // Austria
-    'BE', // Belgium
-    'BG', // Bulgaria
-    'HR', // Croatia
-    'CY', // Cyprus
-    'CZ', // Czech Republic
-    'DK', // Denmark
-    'EE', // Estonia
-    'FI', // Finland
-    'FR', // France
-    'DE', // Germany
-    'GR', // Greece
-    'HU', // Hungary
-    'IE', // Ireland
-    'IT', // Italy
-    'LV', // Latvia
-    'LT', // Lithuania
-    'LU', // Luxembourg
-    'MT', // Malta
-    'NL', // Netherlands
-    'PL', // Poland
-    'PT', // Portugal
-    'RO', // Romania
-    'SK', // Slovakia
-    'SI', // Slovenia
-    'ES', // Spain
-    'SE', // Sweden
+  "AL", // Albania
+  "AD", // Andorra
+  "AM", // Armenia
+  "AT", // Austria
+  "AZ", // Azerbaijan
+  "BY", // Belarus
+  "BE", // Belgium
+  "BA", // Bosnia & Herzegovina
+  "BG", // Bulgaria
+  "HR", // Croatia
+  "CY", // Cyprus
+  "CZ", // Czech Republic
+  "DK", // Denmark
+  "EE", // Estonia
+  "FI", // Finland
+  "FR", // France
+  "GE", // Georgia
+  "DE", // Germany
+  "GR", // Greece
+  "HU", // Hungary
+  "IS", // Iceland
+  "IE", // Ireland
+  "IT", // Italy
+  "KZ", // Kazakhstan
+  "XK", // Kosovo
+  "LV", // Latvia
+  "LI", // Liechtenstein
+  "LT", // Lithuania
+  "LU", // Luxembourg
+  "MT", // Malta
+  "MD", // Moldova
+  "MC", // Monaco
+  "ME", // Montenegro
+  "NL", // Netherlands
+  "MK", // North Macedonia
+  "NO", // Norway
+  "PL", // Poland
+  "PT", // Portugal
+  "RO", // Romania
+  "RU", // Russia
+  "SM", // San Marino
+  "RS", // Serbia
+  "SK", // Slovakia
+  "SI", // Slovenia
+  "ES", // Spain
+  "SE", // Sweden
+  "CH", // Switzerland
+  "TR", // Turkey
+  "UA", // Ukraine
+  "GB", // United Kingdom
+  "VA", // Vatican City
 ];
+
 const config = {
     // Domain restriction
     allowedDomains: ['dev-rpractice.pantheonsite.io', 'assistenzaelettrodomestici-firenze.com'],
